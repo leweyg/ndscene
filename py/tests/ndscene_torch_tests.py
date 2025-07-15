f2ed085fcd5e31677602ff84a3606c26aec04476
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2025-07-15 16:36:28 -0700

    Setting up Torch example data

--- py/tests/ndscene_torch_tests.py
+++ py/tests/ndscene_torch_tests.py
@@ -2,6 +2,38 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+# Define some simple image tools
+class SimpleImages():
+    def __init__(self):
+        size = 4
+        self.target_list = SimpleImages._simpleImageData(size)
+        self.target_tensor = torch.tensor( self.target_list ).float()
+        self.target_flat = self.target_tensor.flatten()
+        self.input_coords = SimpleImages._simpleCoordData(size)
+        self.input_tensor = torch.tensor(self.input_coords).float()
+        self.input_tensor = self.input_tensor.reshape( [size*size, 2] )
+        pass
+    @staticmethod
+    def _simpleCoordData(size):
+        rows = []
+        for y in range(size):
+            row = []
+            rows.append(row)
+            for x in range(size):
+                col = [ x * 1.0, y * 1.0 ]
+                row.append(col)
+        return rows
+    @staticmethod
+    def _simpleImageData(size):
+        rows = []
+        for y in range(size):
+            row = []
+            rows.append(row)
+            for x in range(size):
+                col = (1.0 if (x > y) else 0.0)
+                row.append(col)
+        return rows
+
 # Define the neural network
 class SimpleNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -18,17 +50,18 @@ class SimpleNN(nn.Module):
 
 # Example usage:
 if __name__ == "__main__":
-    input_dim = 10  # Number of input features
+    input_dim = 2  # Number of input features
     hidden_dim = 50 # Number of neurons in the hidden layer
-    output_dim = 2  # Number of output classes or values
+    output_dim = 1  # Number of output classes or values
 
     # Create an instance of the model
     model = SimpleNN(input_dim, hidden_dim, output_dim)
     print("Model structure:")
     print(model)
 
-    # Create a dummy input tensor
-    dummy_input = torch.randn(1, input_dim) # Batch size of 1
+    # Create dummy input
+    img = SimpleImages()
+    dummy_input = img.input_tensor[0]
 
     # Perform a forward pass
     output = model(dummy_input)
