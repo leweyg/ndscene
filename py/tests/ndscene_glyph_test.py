ac21991653ffc86457be7dc9b4fa2546f9c20f8f
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2025-07-31 00:25:04 -0700

    Simplest text layout and rendering, now port to scene graph

--- py/tests/ndscene_glyph_test.py
+++ py/tests/ndscene_glyph_test.py
@@ -3,15 +3,79 @@ print("Importing...")
 
 # without install:
 import ndscenepy.ndscene as ndscene
+import torch
 
 #import ndscene
 
 print("Defining...")
 
+def layout_text(text_tensor):
+    count = text_tensor.shape[0]
+    semi_struct = {
+        "col":0,
+        "row":1,
+        "glyph":2
+    }
+    state = torch.zeros( [count,len(semi_struct)], dtype=torch.int32 )
+    col = 0
+    row = 0
+    glyph = 0
+    for i in range(count):
+        glyph = text_tensor[i]
+        if (glyph == '\n'):
+            row = row + 1
+            col = 0
+            #continue
+        elif (glyph == '\t'):
+            spaces = 4
+            col = ( col + spaces ) - ( col % spaces )
+            #continue
+        else:
+            col = col + 1
+        state[i,0] = col - 1
+        state[i,1] = row
+        state[i,2] = ord(glyph)
+    print(state)
+    return state
+
+def draw_console(dst, layout):
+    count = layout.shape[0]
+    for i in range(count):
+        x = layout[i,0]
+        y = layout[i,1]
+        g = layout[i,2]
+        dst[y,x] = g
+    return dst
+
+def text_from_console(dst):
+    ans = "";
+    for y in range(dst.shape[0]):
+        for x in range(dst.shape[1]):
+            glyph = dst[y,x]
+            if (glyph == 0):
+                glyph = ' '
+            else:
+                glyph = chr(glyph)
+                if (glyph == '\n'):
+                    glyph = ' '
+                if (glyph == '\t'):
+                    glyph = ' '
+            ans += glyph
+        ans += "\n"
+    return ans
+
 def scene_of_text():
-    text = "This is\na test."
+    text = "This is\na\ttest."
     text_data = ndscene.DataND.from_text(text)
     ans = text_data.ensure_tensor()
+
+    layout = layout_text(ans)
+
+    console = torch.zeros( [3, 10], dtype=torch.uint8 )
+    console = draw_console( console, layout )
+    print(console)
+    print(text_from_console(console))
+
     print("text.shape=", ans.shape)
 
     return ans
