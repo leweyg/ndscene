b9423541c054ccd320be4146bdd84c512e2c397a
Lewey Geselowitz <LeweyGeselowitz@gmail.com>
2025-08-04 12:58:50 -0700

    Initial FreedGo test scenes and data

---
+++ py/examples/ndscene_freed_go.py
@@ -0,0 +1,11 @@
+
+import ndscenepy.ndscene as ndscene
+
+def main_freed_go_test():
+    print("Main Freed Go test...")
+    pass;
+
+
+if __name__ == "__main__":
+    main_freed_go_test()
+
---
+++ py/examples/run_go_test.sh
@@ -0,0 +1,7 @@
+
+set -e # stop on error
+cd "$(dirname "$0")" # cd to script directory
+cd ../
+python3 -m examples.ndscene_freed_go
+
+
